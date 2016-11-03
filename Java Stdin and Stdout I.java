f5e551706fea78dc74ae2f99ff2c48d0898a9541
Jan Helmle <jhelmle@gmx.de>
2016-11-03 20:57:57 +0100

    Java Stdin and Stdout I added

---
+++ Java Stdin and Stdout I.java
@@ -0,0 +1,21 @@
+/*
+    Java Stdin and Stdout I
+    Solution for hackerrank challenge
+    Language : Java
+    Author : Jan Helmle
+ */
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
