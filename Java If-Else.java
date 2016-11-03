1b1f6618dc90d13895402041bff4479e97b5649a
Jan Helmle <jhelmle@gmx.de>
2016-11-03 20:49:22 +0100

    Java If-Else added

---
+++ Java If-Else.java
@@ -0,0 +1,37 @@
+/*
+    Java If-Else
+    Solution for hackerrank challenge
+    Language : Java
+    Author : Jan Helmle
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ans = "";
+        if (n % 2 == 1) { // n odd
+            ans = "Weird";
+        } else { // n even
+            if ((n >= 2) && (n <= 5)) {
+                ans = "Not Weird";
+            }
+            if ((n >= 6) && (n <= 20)) {
+                ans = "Weird";
+            }
+            if (n > 20) {
+                ans = "Not Weird";
+            }
+        }
+
+        System.out.println(ans);
+
+    }
+}
