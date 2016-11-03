43d0a22eacbca50a7f1aaf8ef40d3bd9d3efb636
Jan Helmle <jhelmle@gmx.de>
2016-11-03 20:38:54 +0100

    Welcome to Java! added

---
+++ Welcome to Java!.java
@@ -0,0 +1,14 @@
+/*
+    Solution for hackerrank challenge
+    Language : Java
+    Author : Jan Helmle
+*/
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+
