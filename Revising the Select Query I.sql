d3a70c559892b3782416bc0a16882683a59ce34e
Jan Helmle <jhelmle@gmx.de>
2016-11-04 16:21:50 +0100

    Revising the Select Query I added

---
+++ Revising the Select Query I.sql
@@ -0,0 +1,8 @@
+/* 
+   Revising the Select Query I 
+   Solution for hackerrank challenge
+   Language : MySQL
+   Author : Jan Helmle 
+*/
+select * from CITY
+where population > 100000 and countrycode = 'USA'
